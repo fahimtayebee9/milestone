5372ad359de692d504ee5c553f25365410bde728
fahimtayebee9 <fahimtayebee9@gmail.com>
2021-10-26 00:26:43 +0600

    list preview & modal fixing

--- modules/element.js
+++ modules/element.js
@@ -1,28 +1,4 @@
-export default function Element(){
-    Element.mainDiv           = document.querySelector('.main');
-    Element.searchField       = document.querySelector('#searchField');
-    Element.titleBnr          = document.querySelector('#title');
-    Element.circle            = document.querySelector('circle');
-    Element.wrapperBnr        = document.querySelector('.mvList-body');
-    Element.btnSearch         = document.querySelector('#btn-search');
-    Element.btnDomain         = document.querySelector('#btnDomain');
-    Element.menuList          = document.querySelectorAll('.menu-ref');
-    Element.paginationUl      = document.querySelector('#pagination-ul');
-    Element.prevBtnCon        = document.querySelector('#prevBtn-con');
-    Element.prevBtn           = document.querySelector('#prevBtn');
-    Element.nextBtnCon        = document.querySelector('#nextBtn-con');
-    Element.nextBtn           = document.querySelector('#nextBtn');
-    Element.pageNo            = document.querySelectorAll('.page-no');
-    Element.genreBox          = document.querySelector('#genre-Box');
-    Element.movieBox          = document.querySelectorAll('.movieBox');
-    Element.genreTitle        = document.querySelector('.genre-title');
-    Element.ratingBox         = document.querySelector('.rating-box');
-    Element.preloader         = document.querySelector('#preLoader-dv');
-    Element.movideDetails_list= document.querySelector('#movideDetails_list');
-    Element.compCount         = document.querySelector('.comp-count');
-    Element.compBtn           = document.querySelector('.comp-btn');
-    Element.compRef           = document.querySelector('.comp-ref');
-    Element.compModal         = document.querySelector('.comp-modal');
-    Element.compareBox        = document.querySelector('#compareBox');
-    Element.dropDown          = document.querySelector('.dropdown-cart');
+export default class Element {
+    static galleryDiv        = document.querySelector('.gallery'); 
+    static galleryModals     = document.querySelector('#galleryModals'); //
 }
